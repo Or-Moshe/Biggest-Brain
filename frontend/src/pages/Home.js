96c5ab5b69e6926137db9e40adf29be11a2fbcd6
OrMoshe <ormoshe2468@gmail.com>
2025-01-12 13:43:58 +0200

    init

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomeScene from "../scenes/HomeScene";
+
+const Home = () => {
+    
+
+    return (
+        <div>
+            <HomeScene/>
+        </div>
+    );
+}
+
+export default Home;
